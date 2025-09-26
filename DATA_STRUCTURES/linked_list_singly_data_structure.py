6ee786a76ff7cc059e6af0733acd67507acda278
HexaByte <vincentgilceriola3@gmail.com>
2025-09-26 18:00:04 +0800

    implementing the delete method w/ a 'key' parameter

--- DATA_STRUCTURES/linked_list_singly_data_structure.py
+++ DATA_STRUCTURES/linked_list_singly_data_structure.py
@@ -32,6 +32,24 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def delete(self, key):
+        current_node = self.head
+
+        if current_node and current_node.data == key:
+            self.head = current_node.next
+            return
+
+        previous_node = None
+        while current_node and current_node != key:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            print("Key doesn't exists inside the list")
+            return
+
+        previous_node.next = current_node.next
+
     def print_list(self):
         current_node = self.head
 
