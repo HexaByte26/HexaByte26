2037137f787a2bcd44b09c7f41100aa4215bafa6
HexaByte <vincentgilceriola3@gmail.com>
2025-10-03 20:49:08 +0800

    did some changes

--- DATA_STRUCTURES/stack_application.py
+++ DATA_STRUCTURES/stack_application.py
@@ -39,7 +39,7 @@ class DiskStorageStack:
         print(f"Log: Disk {self.disks[self.top_index]} has been popped from the Disk Storage")
 
     def peek_top_disk(self):
-        print(f"Log: {self.disks[self.top_index]} is the top most disk")
+        print(f"Log: {self.disks[self.top_index - 1]} is the top most disk")
 
     def get_disks_num(self):
         print(f"Log: Currently there are {self.top_index} disks inside of the Disk Storage")
@@ -51,23 +51,23 @@ class DiskStorageStack:
         print("\n----B-O-T-T-O-M-----")
 
 
-disk_storage = DiskStorageStack(10)
-disk_storage.push_disk("Harry Potter")
-disk_storage.push_disk("Maze Runner")
-disk_storage.push_disk("Hangover")
+# disk_storage = DiskStorageStack(10)
+# disk_storage.push_disk("Harry Potter")
+# disk_storage.push_disk("Maze Runner")
+# disk_storage.push_disk("Hangover")
 
-disk_storage.show_disks()
+# disk_storage.show_disks()
 
-disk_storage.pop_disk()
+# disk_storage.pop_disk()
 
-disk_storage.peek_top_disk()
+# disk_storage.peek_top_disk()
 
-disk_storage.pop_disk()
-disk_storage.show_disks()
-disk_storage.pop_disk()
-disk_storage.show_disks()
-disk_storage.pop_disk()
-disk_storage.show_disks()
+# disk_storage.pop_disk()
+# disk_storage.show_disks()
+# disk_storage.pop_disk()
+# disk_storage.show_disks()
+# disk_storage.pop_disk()
+# disk_storage.show_disks()
 
 import os, time
 
@@ -88,37 +88,39 @@ def main():
 
         disk_storage = DiskStorageStack(capacity)
 
-        print("=========== Disk Storage Management ===========")
-        for num, operation in enumerate(operations, start=1):
-            print(f"{num}.{ operation}")
-
-        try:
-            selected_operation = int(input("> Enter a number to select an operation: "))
-        except ValueError:
-            continue
-
-        if selected_operation == 1:
-            disk = input("> Enter the name of the disk: ")
-            disk_storage.push_disk(disk)
-
-        elif selected_operation == 2:
-            disk_storage.pop_disk()
-
-        elif selected_operation == 3:
-            disk_storage.peek_top_disk()
-
-        elif selected_operation == 4:
-            disk_storage.get_disks_num()
-
-        elif selected_operation == 5:
+        while running:
+            print("=========== Disk Storage Management ===========")
             disk_storage.show_disks()
+            for num, operation in enumerate(operations, start=1):
+                print(f"{num}.{ operation}")
 
-        elif selected_operation == 6:
-            running = False
+            try:
+                selected_operation = int(input("> Enter a number to select an operation: "))
+            except ValueError:
+                continue
 
-        else:
-            print("Log: Input must be 1 - 6")
-            continue
+            if selected_operation == 1:
+                disk = input("> Enter the name of the disk: ")
+                disk_storage.push_disk(disk)
+
+            elif selected_operation == 2:
+                disk_storage.pop_disk()
+
+            elif selected_operation == 3:
+                disk_storage.peek_top_disk()
+
+            elif selected_operation == 4:
+                disk_storage.get_disks_num()
+
+            elif selected_operation == 5:
+                disk_storage.show_disks()
+
+            elif selected_operation == 6:
+                running = False
+
+            else:
+                print("Log: Input must be 1 - 6")
+                continue
 
 if __name__ == "__main__":
     main()
