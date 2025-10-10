ec8763aca8ca83afb006b7b3aa47c7c58272da01
HexaByte <vincentgilceriola3@gmail.com>
2025-10-10 10:45:14 +0800

    creating files 2

--- DATA_STRUCTURES/queue_application.py
+++ DATA_STRUCTURES/queue_application.py
@@ -1,20 +1,86 @@
-# Toll Gate
+# Toll Gate Queue Manager
+class Vehicle:
+    def __init__(self, name, vehicle_type, brand):
+        self.name = name
+        self.vehicle_type = vehicle_type
+        self.brand = brand
+
+
+class VehicleNode:
+    def __init__(self, vehicle) -> None:
+        self.vehicle = vehicle
+        self.next_vehicle = None
+
 
 class TollGate:
-    def __init__(self, max_size):
-        "param:"
-        "   max_size - max size of the queue"
-        self.toll_queue_max_size = max_size
-        self.toll_queue = ["___"] * self.toll_queue_max_size
-        self.toll_rear = -1
-        self.toll_front = -1
+    def __init__(self):
+        self.toll_queue = None
+        self.toll_rear = None
+        self.toll_front = None
         self.toll_queue_count = 0
 
-    def is_empty(self):
+    def is_toll_empty(self):
         return self.toll_queue_count == 0
     
-    def is_full(self):
-        return self.toll_queue_count == self.toll_queue_max_size
-    
     def enqueue_vehicle(self, vehicle):
+        new_vehicle = VehicleNode(vehicle)
+        if self.is_toll_empty():
+            self.toll_queue = new_vehicle
+            self.toll_front = new_vehicle
+            self.toll_rear = new_vehicle
+            return
         
+        current_vehicle = self.toll_queue
+        while current_vehicle.next_vehicle:
+            current_vehicle = current_vehicle.next_vehicle
+
+        current_vehicle.next_vehicle = new_vehicle
+        self.toll_rear = new_vehicle
+        self.toll_queue_count += 1
+
+    def dequeue_vehicle(self):
+        if self.is_toll_empty():
+            print("Log: Toll Gate is empty")
+            return
+
+        self.toll_queue = self.toll_queue.next_vehicle
+        self.toll_front = self.toll_queue
+        self.toll_queue_count -= 1
+
+    def get_toll_front(self):
+        return self.toll_front
+    
+    def get_toll_rear(self):
+        return self.toll_rear
+    
+    def show_toll_queue(self):
+        if self.is_toll_empty():
+            print("Log: Toll is empty")
+            return
+        
+        current_vehicle = self.toll_queue
+        while current_vehicle.next_vehicle:
+            print(current_vehicle.vehicle, end="->")
+            current_vehicle = current_vehicle.next_vehicle
+
+        print(current_vehicle.vehicle)
+        print(f"First Vehicle of Toll Queue: {self.get_toll_front()}\nDetails:\nName: {self.get_toll_front().vehicle.name}\nVehicle Type: {self.get_toll_front().vehicle.vehicle_type}\nBrand: {self.get_toll_front().vehicle.brand}")
+        print(f"Last Vehicle of Toll Queue: {self.get_toll_rear()}\nDetails:\nName: {self.get_toll_rear().vehicle.name}\nVehicle Type: {self.get_toll_rear().vehicle.vehicle_type}\nBrand: {self.get_toll_rear().vehicle.brand}")
+
+
+def main():
+    operations = ["Add Vehicle to Toll Queue", "Remove Vehicle from Toll Queue", "Show the Queue of the Toll", "Quit"]
+    running = False
+    toll_gate_manager = TollGate()
+
+    while running:
+        print("=============== Toll Gate Manager ===============")
+        print("Toll Gate Queue:")
+        toll_gate_manager.show_toll_queue()
+
+        for num, operation in enumerate(operations, start=1):
+            print(f"{num}. {operation}")
+
+
+if __name__ == "__main__":
+    main()
